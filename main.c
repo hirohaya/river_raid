51edd6af2df0c5d98f015d1cc4fd5bac5af8b5dc
Lucas Hiroshi Hayashida <hiroshi.hayashida@gmail.com>
2014-05-21 00:54:31 -0300

    Macros moved to elementos.h

--- main.c
+++ main.c
@@ -1,21 +1,13 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-#include "elementos.h"
 #include "cenario.h"
-
-#define T_TELA 50
-#define T_MIN_MARGEM 1
-#define T_MAX_MARGEM 10
-#define SEMENTE 424242
 
 int main(int argc, char** argv){
 
-    while(0){
-	   
-    cria_linha(T_TELA, SEMENTE, T_MAX_MARGEM,T_MIN_MARGEM);
-   
-    }
+	cria_tela();
+	imprimi_tela();
+
     return 0;
 
 }
