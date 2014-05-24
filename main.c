1eb56b32e156944d063fc2d0df40ac0e8953a5e3
Lucas Hiroshi Hayashida <hiroshi.hayashida@gmail.com>
2014-05-24 02:30:35 -0300

    Criado o Loop de jogo com um controle de FPS. Para para o loop, use CTRL+C no terminal. Resolvido o problema da 'linha fantasma' que aparecia durante os prints, era apenas uma linha que ficava sem conteudo. Movi a atribucao dos tipos de cada no da linha diretamente para a criacao da linha.

--- main.c
+++ main.c
@@ -1,11 +1,31 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <time.h>
+#include <unistd.h>
 
 #include "game.h"
 
+
+
+#define _XOPEN_SOURCE_700
+#define MS_PER_FRAME 1000000/20
+
+
+
 int main(int argc, char** argv){
+	
 
 	game_init();
+
+	/*Loop de jogo, ele vai ficar pra sempre ou ate voce dar CTRL+C*/
+	while(1){
+		/*Achei melhor limpar a tela antes de imprimir o novo frame*/
+		if(system("clear") == 1);
+		update_tela();
+		imprime_tela();
+		/*Aqui e onde controlamos o FPS (Frames Per Second) do nosso jogo, quanto mais FPS's, mais suaves sao os movimentos*/
+		usleep(MS_PER_FRAME);
+	}
 
     return 0;
 
