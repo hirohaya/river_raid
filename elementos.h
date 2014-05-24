77f7745d903c41f861cbdedc44b839f3cc53387a
Lucas Hiroshi Hayashida <hiroshi.hayashida@gmail.com>
2014-05-24 02:13:35 -0300

    Modificadas os nomes das variaveis de velocidade e posicao. Foram removidas as variaveis de orientacao por nao serem utilizadas.

--- elementos.h
+++ elementos.h
@@ -1,9 +1,9 @@
 #ifndef H_ELEMENTOS
 #define H_ELEMENTOS
 
-#define T_ALT 50
+#define T_ALT 120
 #define T_LARG 70
-#define MIN_MARGEM 6
+#define MIN_MARGEM 5
 #define MAX_MARGEM 20
 
 
@@ -13,23 +13,23 @@ typedef enum tipo{NAVE, TIRO, DEFESA, CORREDOR, SOLIDO} Tipo;
 
 typedef struct defesa {
 
-    int posicao_h, posicao_v, posicao_c;
+    int p_x;
+    int p_y;
+    int p_z;
     int precisao;
     int dano;
     int frequencia_atq;
-    int vel_atq;
 } Defesa;
 
 
 /*ainda nao usaremos posicao_c e as orientacoes*/
 typedef struct nave{
-    int posicao_h;
-    int posicao_v;
-    int posicao_c;
-    int velocidade;
-    int orientacao_h;
-    int orientacao_v;
-    int orientacao_c;
+    int p_x;
+    int p_y;
+    int p_z;
+    int v_x;
+    int v_y;
+    int v_z;
     int dano;
     int hp;
     int powerup;
@@ -40,13 +40,12 @@ typedef struct nave{
 /*ainda nao usaremos posicao_c e as orientacoes*/
 
 typedef struct tiro {
-    int posicao_h;
-    int posicao_v;
-    int posicao_c;
-    int orientacao_h;
-    int orientacao_v;
-    int orientacao_c;
-    int velocidade;
+    int p_x;
+    int p_y;
+    int p_z;
+    int v_x;
+    int v_y;
+    int v_z;
     int dano_atk;
 } Tiro;
 
@@ -66,9 +65,12 @@ typedef struct celula {
 
 /* Linha que será impressa no terminal, nos da lista */
 typedef struct linha {
+
     Celula linha[T_LARG];
     int margem_d;       /*Tamanho da margem direita*/
     int margem_e;       /*Tamanho da margem esquerda*/
+    struct linha *prox;
+    struct linha *ant;
 } Linha;
 
 
